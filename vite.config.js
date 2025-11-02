7684526948ec0b334439d743ec1231c86d1db781
arga1234 <argawirawan1234@gmail.com>
2025-11-03 02:55:02 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +9,39 @@ export default defineConfig({
             input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        react(),
+        react({
+             babel: {
+                plugins: [
+                    'babel-plugin-macros',
+                    [
+                        'babel-plugin-twin',
+                        {
+                        preset: 'styled-components',
+                        },
+                    ],
+                ],
+            },
+        }),
+        svgr({
+            include: '**/*.svg',
+            exclude: [
+                '**/Images/email-illustration.svg',
+            ],
+            svgrOptions: {
+                icon: false,
+            },
+        })
     ],
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx',
+            },
+        },
+    },
+    define: {
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+        'process.env.BABEL_ENV': JSON.stringify(process.env.BABEL_ENV || 'development'),
+        'process.versions.node': JSON.stringify('16.0.0'), 
+    },
 });

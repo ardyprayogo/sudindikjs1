f9fedebfc24a086653cc98003a2d0dfe17b0f8c3
ardy.prayogo <ardy.prayogo@yahoo.com>
2025-11-03 10:53:10 +0700

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,54 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react({
-             babel: {
+            babel: {
                 plugins: [
-                    'babel-plugin-macros',
+                    "babel-plugin-macros",
                     [
-                        'babel-plugin-twin',
+                        "babel-plugin-twin",
                         {
-                        preset: 'styled-components',
+                            preset: "styled-components",
                         },
                     ],
                 ],
             },
         }),
         svgr({
-            include: '**/*.svg',
-            exclude: [
-                '**/Images/email-illustration.svg',
-            ],
+            include: "**/*.svg",
+            exclude: ["**/Images/email-illustration.svg"],
             svgrOptions: {
                 icon: false,
             },
-        })
+        }),
     ],
     optimizeDeps: {
         esbuildOptions: {
             loader: {
-                '.js': 'jsx',
+                ".js": "jsx",
             },
         },
     },
     define: {
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-        'process.env.BABEL_ENV': JSON.stringify(process.env.BABEL_ENV || 'development'),
-        'process.versions.node': JSON.stringify('16.0.0'), 
+        // "process.env.NODE_ENV": JSON.stringify(
+        //     process.env.NODE_ENV || "development"
+        // ),
+        // "process.env.BABEL_ENV": JSON.stringify(
+        //     process.env.BABEL_ENV || "development"
+        // ),
+        "process.versions.node": JSON.stringify("22.16.0"),
+        "process.versions": JSON.stringify("test"),
+        "process.platform": JSON.stringify(process.platform),
+    },
+    build: {
+        chunkSizeWarningLimit: 5000,
     },
 });
